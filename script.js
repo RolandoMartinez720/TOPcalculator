4c1d2e337a0812759fe7a9e613dc168b68377f0a
Rolando Martinez <86631366+RolandoMartinez720@users.noreply.github.com>
2022-03-21 14:47:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('hello world');
+//==function declarations==//
+//==make them able to handle multiple arguments unless not possible ei exponents ** maybe not as the action key will call the function each time so i dont think this is necessary
+
+
+function addNumbers(...num){
+
+  let sum=num.reduce((prev,curr)=>
+      prev+curr,0);
+
+return sum;
+}
+
+function subtract(a,b){
+    return a-b
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function exponent(a,b){
+    return a**b;
+}
+console.log(subtract(2,3));
