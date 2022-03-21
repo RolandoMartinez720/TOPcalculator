bc904a9f4718a14e7ef999ba81b9cc5a60ed1cf1
Rolando Martinez <86631366+RolandoMartinez720@users.noreply.github.com>
2022-03-21 15:25:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+
+let numBtns=Array.from(document.getElementsByClassName('number')); //number buttons
+let displayBox=document.querySelector('.display-box'); //text display box on top of number buttons in calc
+
+numBtns.forEach((element)=>//add event listeners to each button
+element.addEventListener('click', populate)
+)
 //==function declarations==//
 //==make them able to handle multiple arguments unless not possible ei exponents ** maybe not as the action key will call the function each time so i dont think this is necessary
+
+function populate(e){ //takes the target value from the number button pressed
+    console.log(e.target.value);
+    displayBox.value+=e.target.value;
+}
 
 
 function addNumbers(...num){
