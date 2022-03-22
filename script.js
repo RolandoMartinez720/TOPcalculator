a3c4c1d285ebd83a2286811c8f2255890f41fea4
Rolando Martinez <86631366+RolandoMartinez720@users.noreply.github.com>
2022-03-22 14:34:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 
 let numBtns=Array.from(document.getElementsByClassName('number')); //number buttons
 let displayBox=document.querySelector('.display-box'); //text display box on top of number buttons in calc
+let operators=Array.from(document.getElementsByClassName('operation'));
+
+
+let a, b, operation; //numbers 1 and 2
+
+//when operator key is pressed we need to:
+//set value of operator to key pressed
+//set value of number1 (a) to value in the text box
+//clear value of the box
+
+operators.forEach(element=>element.addEventListener('click', function(e){
+    a=displayBox.value;
+    console.log(a);
+    operation=e.target.value;
+    console.log(operation);
+}))
+
+//when equals key is pressed we need to:
+//set value of number2(b) to value in the text box
+//call the calculate function with the values of a,b, operator
+//set display box value to the returned value from calculate function
+
 
 numBtns.forEach((element)=>//add event listeners to each button
 element.addEventListener('click', populate)
@@ -14,18 +36,11 @@ function populate(e){ //takes the target value from the number button pressed
 }
 
 
-function add(a,b){
-    return a+b;
-}
+function calculate(a,b,operation){
 
-function subtract(a,b){
-    return a-b
-}
+    if(operation=`+`) return a+b;
+    if(operation=`-`) return a-b;
+    if(operation=`*`) return a*b;
+    if(operation=`/`) return a/b;
 
-function multiply(a,b){
-    return a*b;
-}
-
-function divide(a,b){
-    return a/b;
 }
