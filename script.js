2874c32652e352cdd1587a95407dd340b024e8c7
Rolando <rolando.a.martinez@outlook.com>
2022-03-22 16:47:46 -0500

    completed odin project calculator

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let numBtns=Array.from(document.getElementsByClassName('number')); //number buttons
 let displayBox=document.querySelector('.display-box'); //text display box on top of number buttons in calc
 let operators=Array.from(document.getElementsByClassName('operation'));
-
+let equalBtn=document.querySelector('.equals');
 
 let a, b, operation; //numbers 1 and 2
 
@@ -16,12 +16,20 @@ operators.forEach(element=>element.addEventListener('click', function(e){
     console.log(a);
     operation=e.target.value;
     console.log(operation);
+    displayBox.value='';
 }))
 
 //when equals key is pressed we need to:
 //set value of number2(b) to value in the text box
 //call the calculate function with the values of a,b, operator
 //set display box value to the returned value from calculate function
+
+equalBtn.addEventListener('click',function(e){
+    b=displayBox.value;
+    displayBox.value=calculate(a,b,operation);
+    a=displayBox.value;
+})
+
 
 
 numBtns.forEach((element)=>//add event listeners to each button
@@ -31,16 +39,26 @@ element.addEventListener('click', populate)
 //==make them able to handle multiple arguments unless not possible ei exponents ** maybe not as the action key will call the function each time so i dont think this is necessary
 
 function populate(e){ //takes the target value from the number button pressed
-    console.log(e.target.value);
     displayBox.value+=e.target.value;
 }
 
 
 function calculate(a,b,operation){
+    a=Number(a);
+    b=Number(b);
+    
+    if(operation==`+`) return a+b;
+    if(operation==`-`) return a-b;
+    if(operation==`*`) return a*b;
+    if(operation==`/`) return a/b;
 
-    if(operation=`+`) return a+b;
-    if(operation=`-`) return a-b;
-    if(operation=`*`) return a*b;
-    if(operation=`/`) return a/b;
+}
 
+//clear all function and event listener
+document.querySelector('.clear').addEventListener('click',clearAll);
+
+function clearAll(){
+    a='';
+    b='';
+    displayBox.value='';
 }
